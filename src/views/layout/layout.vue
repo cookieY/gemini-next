fb90a4d2bbe9f785b6388f4d0256a48df2d0b110
HenryYee <henry@yearning.io>
2022-07-21 17:42:55 +0800

    修复菜单布局样式异常的问题

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,58 +1,62 @@
 <template>
       <div v-watermark="{ text: store.state.user.account.user }">
+            <a-layout>
+                  <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+                        <img :src="logoUrl" style="width: 150px;" />
+                        <Menu></Menu>
+                  </a-layout-sider>
 
-            <a-layout-sider :style="{ overflow: 'auto', minHeight: '100vh', position: 'fixed', left: 0 }">
-                  <img :src="logoUrl" style="width: 150px;" />
-                  <Menu></Menu>
-            </a-layout-sider>
-
-            <a-layout :style="{ marginLeft: '200px' }">
-                  <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-                        <a-row>
-                              <a-col :span="2">
-                                    <a-button @click="toggle" type="text">
-                                          <template #icon v-if="isFullscreen">
-                                                <FullscreenExitOutlined />
-                                          </template>
-                                          <template #icon v-else>
-                                                <FullscreenOutlined />
-                                          </template>
-                                    </a-button>
-                              </a-col>
-                              <a-col :span="5" :offset="17">
-                                    <a-dropdown>
-                                          <div>
-                                                <a-avatar :src="profile" />
-                                                <span style="margin-left: 5%;font-weight: bold;">{{
-                                                            store.state.user.account.user
-                                                }}</span>
-                                          </div>
-                                          <template #overlay>
-                                                <a-menu @click="() => router.push({ path: '/home/profile' })">
-                                                      <a-menu-item>
-                                                            <a href="javascript:;">个人详情</a>
-                                                      </a-menu-item>
-                                                </a-menu>
-                                          </template>
-                                    </a-dropdown>
-                              </a-col>
-                        </a-row>
-                  </a-layout-header>
-                  <br />
-                  <br />
-                  <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
-                        <router-view v-slot="{ Component }">
-                              <component :is="Component" />
-                        </router-view>
-                  </a-layout-content>
-                  <a-layout-footer :style="{ textAlign: 'center', width: '100%' }">{{
-                              Copyright
-                  }}
-                  </a-layout-footer>
+                  <a-layout>
+                        <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+                              <a-row>
+                                    <a-col :span="1">
+                                          <menu-unfold-outlined v-if="collapsed" class="trigger"
+                                                @click="() => (collapsed = !collapsed)" />
+                                          <menu-fold-outlined v-else class="trigger"
+                                                @click="() => (collapsed = !collapsed)" />
+                                    </a-col>
+                                    <a-col :span="1">
+                                          <a-button @click="toggle" type="text">
+                                                <template #icon v-if="isFullscreen">
+                                                      <FullscreenExitOutlined />
+                                                </template>
+                                                <template #icon v-else>
+                                                      <FullscreenOutlined />
+                                                </template>
+                                          </a-button>
+                                    </a-col>
+                                    <a-col :span="7" :offset="15">
+                                          <a-dropdown>
+                                                <div>
+                                                      <a-avatar :src="profile" />
+                                                      <span style="font-weight: bold;">{{
+                                                                  store.state.user.account.user
+                                                      }}</span>
+                                                </div>
+                                                <template #overlay>
+                                                      <a-menu @click="() => router.push({ path: '/home/profile' })">
+                                                            <a-menu-item>
+                                                                  <a href="javascript:;">个人详情</a>
+                                                            </a-menu-item>
+                                                      </a-menu>
+                                                </template>
+                                          </a-dropdown>
+                                    </a-col>
+                              </a-row>
+                        </a-layout-header>
+                        <br />
+                        <br />
+                        <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+                              <router-view v-slot="{ Component }">
+                                    <component :is="Component" />
+                              </router-view>
+                        </a-layout-content>
+                        <a-layout-footer :style="{ textAlign: 'center', width: '100%' }">{{
+                                    Copyright
+                        }}
+                        </a-layout-footer>
+                  </a-layout>
             </a-layout>
-
-
-
             <a-drawer placement="right" :closable="false" :visible="is_open" @close="close">
                   <Menu @close="() => is_open = false"></Menu>
             </a-drawer>
@@ -66,8 +70,9 @@ import { overrideHeaders } from "@/config/request"
 import { useStore } from "@/store";
 import profile from "@/assets/comment/3.svg"
 import { useRouter } from "vue-router";
-import { FullscreenExitOutlined, FullscreenOutlined } from "@ant-design/icons-vue";
+import { FullscreenExitOutlined, MenuUnfoldOutlined, MenuFoldOutlined, FullscreenOutlined } from "@ant-design/icons-vue";
 import { useFullscreen } from '@vueuse/core'
+import { ref } from "vue";
 
 const logoUrl = localStorage.getItem("theme") === "light" ? new URL("../../assets/login/logo.jpeg", import.meta.url).href : new URL("../../assets/login/logo.png", import.meta.url).href
 
@@ -77,6 +82,7 @@ const store = useStore()
 
 const router = useRouter()
 
+const collapsed = ref<boolean>(false)
 
 
 
