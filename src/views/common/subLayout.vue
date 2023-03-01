b73df685aae945c55be5abe9fd6c55c633bd0716
HenryYee <henry@yearning.io>
2023-03-01 16:47:54 +0800

    取消前端缓存

--- src/views/common/subLayout.vue
+++ src/views/common/subLayout.vue
@@ -1,14 +1,10 @@
 <template>
-  <router-view v-slot="{ Component, route }">
-    <keep-alive>
-      <component
-        :is="Component"
-        v-if="route.meta.keepAlive"
-        :key="route.path"
-      />
-    </keep-alive>
-    <component :is="Component" v-if="!route.meta.keepAlive" />
-  </router-view>
+      <router-view v-slot="{ Component, route }">
+            <!-- <keep-alive v-if="route.meta.keepAlive">
+                  <component :is="Component" :key="route.path" />
+            </keep-alive> -->
+            <component :is="Component" :key="route.path" />
+      </router-view>
 </template>
 
 <script lang="ts" setup></script>
